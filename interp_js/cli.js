ad73bbf4b06170b2a1a84289652e3d3bd75930b5
jasondunken <jasondunken@yahoo.com>
2023-05-22 21:38:02 -0400

    implement repl

--- interp_js/cli.js
+++ interp_js/cli.js
@@ -2,24 +2,67 @@ import { Tokenizer } from "./tokenizer.js";
 import { Parser } from "./parser.js";
 
 import * as fs from "fs";
+import * as readline from "readline";
 
 console.log("***Monkey Code Interpreter_JS***");
 const inputFile = process.argv[2];
-const flags = process.argv.slice(3);
+const args = process.argv.slice(3);
 
-console.log("loading: ", inputFile, "...");
+class Monkey {
+    constructor(inputFile, args) {
+        this.reader = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout,
+        });
 
-fs.readFile(inputFile, "utf8", (err, data) => {
-    if (err) {
-        console.error("ERROR====>>>> ", err);
-        return;
+        if (inputFile) {
+            this.loadingFile = true;
+            this.loadFile(inputFile);
+        }
+
+        if (!this.loadingFIle) {
+            this.loop();
+        }
     }
-    doTheMonkey(data);
-});
 
-function doTheMonkey(inputString) {
-    const tokenizer = new Tokenizer(inputString);
-    tokenizer.test();
-    //const parser = new Parser(tokenizer);
-    //const program = parser.parse();
+    loop() {
+        this.reader.question(`Monkey$ `, (command) => {
+            this.handleCommand(command);
+        });
+    }
+
+    handleCommand(command) {
+        const cmd = command.split(" ");
+        switch (cmd[0]) {
+            case "EXIT":
+                this.reader.close();
+                return;
+            case "load":
+                this.loadFile(cmd[1]);
+                break;
+            default:
+                this.doTheMonkey(command);
+        }
+    }
+
+    loadFile(path) {
+        console.log("loading: ", path, "...");
+        fs.readFile(path, "utf8", (err, data) => {
+            if (err) {
+                console.error("ERROR====>>>> ", err);
+                return;
+            }
+            this.doTheMonkey(data);
+            this.loop();
+        });
+    }
+
+    doTheMonkey(inputString) {
+        const tokenizer = new Tokenizer(inputString);
+        tokenizer.test();
+        //const parser = new Parser(tokenizer);
+        //const program = parser.parse();
+        this.loop();
+    }
 }
+new Monkey(inputFile, args);
