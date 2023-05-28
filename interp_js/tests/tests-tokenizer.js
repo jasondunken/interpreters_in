e250b871ad0ddaf21dd66001e2fe652bbf0489a9
jasondunken <jasondunken@yahoo.com>
2023-05-28 15:32:05 -0400

    extend nextToken test

--- interp_js/tests/tests-tokenizer.js
+++ interp_js/tests/tests-tokenizer.js
@@ -1,17 +1,27 @@
 import { Tokenizer } from "../tokenizer.js";
 import { Tokens } from "../token.js";
 
-import { Log } from "../cli.js";
+import { Log } from "../logger.js";
 
 export function testNextToken() {
-    Log.LogInfo("Tokenizer Test", "test.nextToken");
+    Log.info("Tokenizer Test", "testing nextToken()");
     const input = `
         let five = 5;
         let ten = 10;
         let add = fn(x, y) {
             x + y;
         };
-        let result = add(five, ten);`;
+        let result = add(five, ten);
+        !-/*5;
+        5 < 10 > 5;
+        
+        if (5 < 10) {
+            return true;
+        } else {
+            return false;
+        }
+        
+        `;
 
     const tokenizer = new Tokenizer(input);
     let failed = 0;
@@ -22,11 +32,13 @@ export function testNextToken() {
         [Tokens.ASSIGN, "="],
         [Tokens.INT, "5"],
         [Tokens.SEMICOLON, ";"],
+
         [Tokens.LET, "let"],
         [Tokens.IDENT, "ten"],
         [Tokens.ASSIGN, "="],
         [Tokens.INT, "10"],
         [Tokens.SEMICOLON, ";"],
+
         [Tokens.LET, "let"],
         [Tokens.IDENT, "add"],
         [Tokens.ASSIGN, "="],
@@ -43,6 +55,7 @@ export function testNextToken() {
         [Tokens.SEMICOLON, ";"],
         [Tokens.RBRACE, "}"],
         [Tokens.SEMICOLON, ";"],
+
         [Tokens.LET, "let"],
         [Tokens.IDENT, "result"],
         [Tokens.ASSIGN, "="],
@@ -53,19 +66,53 @@ export function testNextToken() {
         [Tokens.IDENT, "ten"],
         [Tokens.RPAREN, ")"],
         [Tokens.SEMICOLON, ";"],
+
+        [Tokens.BANG, "!"],
+        [Tokens.MINUS, "-"],
+        [Tokens.SLASH, "/"],
+        [Tokens.ASTERISK, "*"],
+        [Tokens.INT, "5"],
+        [Tokens.SEMICOLON, ";"],
+
+        [Tokens.INT, "5"],
+        [Tokens.LT, "<"],
+        [Tokens.INT, "10"],
+        [Tokens.GT, ">"],
+        [Tokens.INT, "5"],
+        [Tokens.SEMICOLON, ";"],
+
+        [Tokens.IF, "if"],
+        [Tokens.LPAREN, "("],
+        [Tokens.INT, "5"],
+        [Tokens.LT, "<"],
+        [Tokens.INT, "10"],
+        [Tokens.RPAREN, ")"],
+        [Tokens.LBRACE, "{"],
+        [Tokens.RETURN, "return"],
+        [Tokens.TRUE, "true"],
+        [Tokens.SEMICOLON, ";"],
+        [Tokens.RBRACE, "}"],
+        [Tokens.ELSE, "else"],
+        [Tokens.LBRACE, "{"],
+        [Tokens.RETURN, "return"],
+        [Tokens.FALSE, "false"],
+        [Tokens.SEMICOLON, ";"],
+        [Tokens.RBRACE, "}"],
+
         [Tokens.EOF, "\0"],
     ];
 
     for (let i = 0; i < tests.length; i++) {
         const test = tests[i];
         const token = tokenizer.nextToken();
-        Log.LogInfo(`Tokenizer Test[${i}]`, `expected ${test[0].token} '${test[1]}' got '${token.literal}'`);
+        Log.info(`Tokenizer Test`, `test[${i}] expected ${test[0].token} '${test[1]}' got '${token.literal}'`);
         if (test[0].token !== token.token) {
-            Log.LogError(`Tokenizer Test[${i}]`, `token type wrong. expected '${test[0].token}' got ${token.token}`);
-            failed++;
+            Log.error(`Tokenizer Test`, `test[${i}] token type wrong. expected '${test[0].token}' got ${token.token}`);
         }
         if (test[1] !== token.literal) {
-            Log.LogError(`Tokenizer Test[${i}]`, `token literal wrong. expected '${test[1]}' got ${token.literal}`);
+            Log.error(`Tokenizer Test`, `test[${i}] token literal wrong. expected '${test[1]}' got ${token.literal}`);
+        }
+        if (test[0].token !== token.token || test[1] !== token.literal) {
             failed++;
         }
     }
